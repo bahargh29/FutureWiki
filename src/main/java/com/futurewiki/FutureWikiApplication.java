aebd7f013c1c9cacd2aff9cd0503a6d8e7e9530e
bahar <baharehghiasy8529@gmail.com>
2026-07-01 18:19:59 +0330

    build: initialize Spring Boot project

---
+++ src/main/java/com/futurewiki/FutureWikiApplication.java
@@ -0,0 +1,13 @@
+package com.futurewiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureWikiApplication.class, args);
+	}
+
+}
